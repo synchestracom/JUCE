37e96211ffd2ca5c0d28ebe1a25e1292541803ed
Mickael BAUDOUIN <mickael.baudouin@synchestra.ai>
2024-04-12 16:15:29 +0200

    (temp) Sort directory contents by update date.

--- modules/juce_gui_basics/filebrowser/juce_DirectoryContentsList.cpp
+++ modules/juce_gui_basics/filebrowser/juce_DirectoryContentsList.cpp
@@ -267,7 +267,10 @@ bool DirectoryContentsList::addFile (const File& file, const bool isDir,
                 return a->isDirectory;
            #endif
 
-            return a->filename.compareNatural (b->filename) < 0;
+            //TODO put back sort by name
+            //return a->filename.compareNatural (b->filename) < 0;
+            
+            return a->modificationTime >= b->modificationTime; 
         });
 
         return true;
