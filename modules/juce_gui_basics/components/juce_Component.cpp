ba67d79b4619b7545fd77cde13edab5fd67b0151
Mickael BAUDOUIN <mickael.baudouin@synchestra.ai>
2024-05-27 11:28:37 +0200

    Revert "Make Component::findChildWithID recursive"
    This reverts commit 9602b07f1d15a7942b6b03ffbe6c6876394d486b.

--- modules/juce_gui_basics/components/juce_Component.cpp
+++ modules/juce_gui_basics/components/juce_Component.cpp
@@ -277,9 +277,6 @@ void Component::setComponentID (const String& newID)
 
 void Component::setVisible (bool shouldBeVisible)
 {
-    if (componentID == "Work Settings")
-        int i = 0;
-    
     if (flags.visibleFlag != shouldBeVisible)
     {
         // if component methods are being called from threads other than the message
@@ -1300,16 +1297,11 @@ int Component::getIndexOfChildComponent (const Component* child) const noexcept
     return childComponentList.indexOf (const_cast<Component*> (child));
 }
 
-Component* Component::findChildWithID (StringRef targetID, bool recursive) const noexcept
+Component* Component::findChildWithID (StringRef targetID) const noexcept
 {
     for (auto* c : childComponentList)
         if (c->componentID == targetID)
             return c;
-        else {
-            if (recursive)
-                if (auto subchild = c->findChildWithID(targetID, recursive); subchild != nullptr)
-                    return subchild;
-        }
 
     return nullptr;
 }
